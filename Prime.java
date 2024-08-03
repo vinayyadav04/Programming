a4f555f8e321baf75cf7a68403618c712506a7b3
vinayyadav04 <vinayyadav04091999@gmail.com>
2024-08-03 21:16:10 +0530

    prime

---
+++ Prime.java
@@ -0,0 +1,22 @@
+//check prime or not
+
+import java.util.Scanner;
+
+class Prime{
+    public static void main(String[] args) {
+    
+        
+        Scanner scn=new Scanner(System.in);
+        int num=scn.nextInt();
+        int count =0;
+        for(int i=2;i<=num/2;i++){
+            if(num%i==0)
+            count++;
+        }
+        if(count>0){
+            System.out.println("Not Prime");
+        }else{
+            System.out.println("Prime");
+        }
+    }
+}
