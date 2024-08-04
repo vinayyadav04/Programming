b0ca696344c1e0406d171be9b98493cf358f21ac
vinayyadav04 <vinayyadav04091999@gmail.com>
2024-08-04 18:51:50 +0530

    GCDHCF

---
+++ GCDHCF.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class GCDHCF {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int num1=scn.nextInt();
+        int num2=scn.nextInt();
+
+        int largest=num1>num2?num1:num2;
+
+        int hcf=1;
+
+        for(int i=largest/2;i>2;i--){
+            if(num1%i==0 && num2%i==0){
+                hcf=i;
+                System.out.println(hcf);
+                break;
+            }
+
+        }
+    }
+}
