dc94ecec8b4f75b925919a295ed48d979877077f
vinayyadav04 <vinayyadav04091999@gmail.com>
2024-08-05 23:20:33 +0530

    LCM

---
+++ LCM.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class LCM {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        //take 2 inputs
+        int num1=scn.nextInt();
+        int num2=scn.nextInt();
+        //finding the larger number
+        int max=num1>num2?num1:num2;
+
+        while (true) {
+            if(max%num1==0 && max%num2==0){
+            System.out.println(max);
+            break;
+            }
+            
+        }
+        max++;
+    }
+}
