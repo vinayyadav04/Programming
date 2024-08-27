63b5fa39961305a72ad12f9af1a0d8626653f179
vinayyadav04 <vinayyadav04091999@gmail.com>
2024-08-27 19:49:32 +0530

    stack

---
+++ PrintPrimeElements.java
@@ -0,0 +1,67 @@
+
+
+import java.util.Scanner;
+
+public class PrintPrimeElements {
+    
+    static class Node {
+        int data;
+        Node next;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Node head = null, tail = null;
+        boolean flag = true;
+
+        
+        while (flag) {
+            Node n = new Node();
+            System.out.println("Enter data:");
+            n.data = s.nextInt();
+
+            if (head == null) {
+                head = n;
+                tail = n;
+            } else {
+                tail.next = n;
+                tail = n;
+            }
+
+            System.out.println("Do you want to create another node? \n1)Yes\n2)No");
+            int ch = s.nextInt();
+            if (ch != 1) {
+                flag = false;
+            }
+        }
+
+       
+        System.out.println("Prime elements in the list:");
+        Node temp = head;
+        while (temp != null) {
+            if (isPrime(temp.data)) {
+                System.out.print(temp.data + " ");
+            }
+            temp = temp.next;
+        }
+    }
+
+    
+    static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n == 2 || n == 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Stack.java
@@ -0,0 +1,20 @@
+public class Stack {
+    
+}
+//local variable so they are stored in stack
+//static variable class area
+//non-static variable heap area
+
+//Stack implimentation
+//1.Stack using Array
+//2.Stack using LinkedList
+
+//Operation
+//push:to insert the data in stack
+//pop: to remove the data from stack
+//peek:used to display the top most element
+//isEmpty():used to check whether stack is empty or not 
+//isFull():used to check whether stack is full or not
+//Display():it will display all element in stack
+
+//top : top is a variable used for traversing
