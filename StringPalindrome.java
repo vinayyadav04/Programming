5713811a8352c77f317729b93d1ba846784e9ddc
vinayyadav04 <vinayyadav04091999@gmail.com>
2024-09-15 14:58:47 +0530

    palindrome String

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -8,9 +8,15 @@ public class StringPalindrome{
 
         String s=scn.next();
         String rev="";
+        scn.close();
         for(int i=s.length()-1;i>=0;i--){
             rev=rev+s.charAt(i);
         }
-        System.out.println(rev);
+        
+        if(s.equals(rev)){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not Palindrome");
+        }
     }
 }
